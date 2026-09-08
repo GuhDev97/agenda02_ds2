b94c55d2e0d1196236b4d27f8eafe61227f7d5b6
Gustavo Dias <gustaavvoo97@gmail.com>
2026-09-07 21:25:10 -0300

    aula da agenda 5ds

---
+++ agenda05/aula1/decArray.php
@@ -0,0 +1,17 @@
+<?php
+    $sudeste = array("São Paulo", "Rio de Janeiro", "Minas Gerais", "Espírito Santo");
+    echo '<h1>Estados do Sudeste</h1>';
+    echo $sudeste[0];
+
+    $filme = array('titulo' => 'O Senhor dos Anéis', 'ano' => 2001, 'diretor' => 'Peter Jackson');
+    echo '<h1>Filme</h1>';
+    echo $filme['titulo']. '<br>'; 
+    echo $filme['ano']. '<br>';
+    echo $filme['diretor']. '<br>';
+
+
+
+?>
+
+
+    
